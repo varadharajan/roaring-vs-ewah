1d2d545384de52b04ff5e9259de4053bd127c4fa
Varadharajan <srinathsmn@gmail.com>
2017-11-17 22:03:11 +0530

    Const Iterator is much faster than Normal iterator

--- main.cxx
+++ main.cxx
@@ -15,10 +15,29 @@ void bench_ewah(int cardinality)
 
     auto counter = 0;
     auto start = std::chrono::steady_clock::now();
-    for(auto i = x.begin() ; i != x.end() ; i++) ++counter;
+    for(auto i = x.begin() ; i != x.end() ; i++) counter += *i;
     auto end = std::chrono::steady_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
     std::cout << "EWAH Entries : " << counter
+        << " Time: " << elapsed.count() << " ns"
+        << " Size: " << x.sizeInBits() / (8 * 1024) << " KB" << std::endl;
+}
+
+void bench_ewah_const_iterator(int cardinality)
+{
+    bitmap x;
+    for(auto i = 0; i <= 100000000; i++) {
+        if(i % cardinality == 0) x.set(i);
+    }
+
+    auto counter = 0;
+    auto start = std::chrono::steady_clock::now();
+    for(EWAHBoolArray<uint64_t>::const_iterator j = x.begin(); j != x.end(); ++j){
+        counter+=*j;
+    }
+    auto end = std::chrono::steady_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    std::cout << "EWAH ConstIterator Entries : " << counter
         << " Time: " << elapsed.count() << " ns"
         << " Size: " << x.sizeInBits() / (8 * 1024) << " KB" << std::endl;
 }
@@ -36,7 +55,7 @@ void bench_roaring(int cardinality)
 
     auto counter = 0;
     auto start = std::chrono::steady_clock::now();
-    for(Roaring::const_iterator i = r.begin() ; i != r.end() ; i++) ++counter;
+    for(Roaring::const_iterator i = r.begin() ; i != r.end() ; i++) counter+=*i;
     auto end = std::chrono::steady_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
     std::cout << "Roaring Entries : " << counter
@@ -49,8 +68,9 @@ int main(int argc, char const *argv[])
 
     for(auto i = 10; i <= 100000; i*=10) {
         std::cout << "Cardinality : 1 in " << i << " bits" << std::endl;
-        bench_roaring(i);
+        bench_ewah_const_iterator(i);
         bench_ewah(i);
+        bench_roaring(i);
         std::cout << std::endl;
     }
     return 0;
